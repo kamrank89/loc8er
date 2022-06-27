e7ddb96a232683de7e6bce0cabe0324eadf9430b
kamrank89 <68290007+kamrank89@users.noreply.github.com>
2022-06-27 11:28:54 -0500

    changes to routers and controllers

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
+const ctrlMain = require("../controllers/main");
 
 /* GET home page. */
-const homepageController = (req, res) =>
-  res.render("index", { title: "Express" });
-router.get("/", homepageController);
+
+router.get("/", ctrlMain.index);
 
 module.exports = router;
